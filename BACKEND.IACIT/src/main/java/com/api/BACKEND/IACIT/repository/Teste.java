de9bc46856fb304d234d003ad8b0c5a221cac91b
Ana Clara Oliveira Leal <90358925+heyanaleal@users.noreply.github.com>
2022-09-03 20:29:45 -0300

    Criação da Conexão com Bd

---
+++ BACKEND.IACIT/src/main/java/com/api/BACKEND/IACIT/repository/Teste.java
@@ -0,0 +1,10 @@
+package com.api.BACKEND.IACIT.repository;
+
+public class Teste {
+
+    public static void main(String[] args) {
+    	ConnectionFactory conexao = new  ConnectionFactory();
+        conexao.conectar();
+    }
+    
+}
