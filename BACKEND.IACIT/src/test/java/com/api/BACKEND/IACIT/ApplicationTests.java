7006bba1df3769463a4f0f28d804627c2f7818c6
Patrick Souza <patrickgabriel103f@gmail.com>
2022-09-02 22:33:19 -0300

    Inicio projeto Spring

---
+++ BACKEND.IACIT/src/test/java/com/api/BACKEND/IACIT/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.BACKEND.IACIT;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
