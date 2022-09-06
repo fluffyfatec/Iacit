80cd47542224a8139e4a2c688a30dd3652b56f4b
Felipe Sobral <felipesobral_@hotmail.com>
2022-09-06 04:59:57 -0300

    Ajustando à POO

--- CSVs/cabeçalho.py
+++ CSVs/cabeçalho.py
@@ -1,34 +1,37 @@
 import pandas as pd
 
 
-def tratamento_cabecalho(df):
+class Cabecalho:
 
-    pd.options.mode.chained_assignment = None
+    @staticmethod
+    def tratamento_cabecalho(df):
 
-    df['REGIAO'] = df.loc[0][1]
-    df['UF'] = df.loc[1][1]
-    df['ESTACAO'] = df.loc[2][1]
-    df['CODIGO (WMO)'] = df.loc[3][1]
-    df['LATITUDE'] = df.loc[4][1]
-    df['LONGITUDE'] = df.loc[5][1]
-    df['ALTITUDE'] = df.loc[6][1]
-    df['DATA DE FUNDACAO'] = df.loc[7][1]
+        pd.options.mode.chained_assignment = None
 
-    # Apagando duplicadas
-    df = df.drop_duplicates(subset=['REGIAO', 'UF'], keep='first')
+        df['REGIAO'] = df.loc[0][1]
+        df['UF'] = df.loc[1][1]
+        df['ESTACAO'] = df.loc[2][1]
+        df['CODIGO (WMO)'] = df.loc[3][1]
+        df['LATITUDE'] = df.loc[4][1]
+        df['LONGITUDE'] = df.loc[5][1]
+        df['ALTITUDE'] = df.loc[6][1]
+        df['DATA DE FUNDACAO'] = df.loc[7][1]
 
-    # Apagando vírgulas extras
-    cols = ["REGIAO", "UF", "ESTACAO", "CODIGO (WMO)", "LATITUDE", "LONGITUDE", "ALTITUDE", "DATA DE FUNDACAO"]
-    df[cols] = df[cols].replace({',,,,,,,,,,,,,': ''}, regex=True)
-    df[cols] = df[cols].replace({',,,,,,,,,,,,': ''}, regex=True)
-    df[cols] = df[cols].replace({',': '.'}, regex=True)
+        # Apagando duplicadas
+        df = df.drop_duplicates(subset=['REGIAO', 'UF'], keep='first')
 
-    # Convertendo a datetime
-    df['DATA DE FUNDACAO'] = pd.to_datetime(df['DATA DE FUNDACAO'], errors='coerce')
+        # Apagando vírgulas extras
+        cols = ["REGIAO", "UF", "ESTACAO", "CODIGO (WMO)", "LATITUDE", "LONGITUDE", "ALTITUDE", "DATA DE FUNDACAO"]
+        df[cols] = df[cols].replace({',,,,,,,,,,,,,': ''}, regex=True)
+        df[cols] = df[cols].replace({',,,,,,,,,,,,': ''}, regex=True)
+        df[cols] = df[cols].replace({',': '.'}, regex=True)
 
-    # Config para visualizar todas as colunas
-    pd.set_option('display.max_columns', None)
+        # Convertendo a datetime
+        df['DATA DE FUNDACAO'] = pd.to_datetime(df['DATA DE FUNDACAO'], errors='coerce')
 
-    return df
+        # Config para visualizar todas as colunas
+        pd.set_option('display.max_columns', None)
+
+        return df
 
 
--- CSVs/leituraDfs.py
+++ CSVs/leituraDfs.py
@@ -1,130 +1,133 @@
 import pandas as pd
 import glob
-from cleaningData import tratamento_dfs
-from conexaoBD import povoar_banco, estacao_banco
-from cabeçalho import tratamento_cabecalho
+from conexaoBD import ConexaoBD
+from cleaningData import CleaningData
+from cabeçalho import Cabecalho
 
 
-def leitura_dfs2020():
+class LeituraDfs:
 
-    # Path dos dataframes
-    files = glob.glob(r"C:\Users\Felipe Sobral\Downloads\Dfs 2020\*.csv")
+    @staticmethod
+    def leitura_dfs2020():
 
-    i = 1
-    for f in files:
-        df = pd.read_csv(f, sep=';', encoding='latin-1', on_bad_lines='skip')
-        df1 = pd.read_csv(f, sep=';', encoding='latin-1', skiprows=[0, 1, 2, 3, 4, 5, 6, 7], on_bad_lines='skip')
+        # Path dos dataframes
+        files = glob.glob(r"C:\Users\Felipe Sobral\Downloads\Dfs 2020\*.csv")
 
-        # Criando colunas do cabeçalho vertical
-        df['CODIGO (WMO)'] = df.loc[2][1]
+        i = 1
+        for f in files:
+            df = pd.read_csv(f, sep=';', encoding='latin-1', on_bad_lines='skip')
+            df1 = pd.read_csv(f, sep=';', encoding='latin-1', skiprows=[0, 1, 2, 3, 4, 5, 6, 7], on_bad_lines='skip')
 
-        # Dando merge nos dataframes
-        df = df1.assign(key=1).merge(df[['CODIGO (WMO)']].assign(key=1), on='key').drop('key', axis=1)
-        del df1
+            # Criando colunas do cabeçalho vertical
+            df['CODIGO (WMO)'] = df.loc[2][1]
 
-        # Tratamento dos dados e povoamento do banco de dados
-        povoar_banco(tratamento_dfs(df))
+            # Dando merge nos dataframes
+            df = df1.assign(key=1).merge(df[['CODIGO (WMO)']].assign(key=1), on='key').drop('key', axis=1)
+            del df1
 
-        print(i)
-        i += 1
+            # Tratamento dos dados e povoamento do banco de dados
+            ConexaoBD.povoar_banco(CleaningData.tratamento_dfs(df))
 
+            print(i)
+            i += 1
 
-def leitura_dfs2021():
+    @staticmethod
+    def leitura_dfs2021():
 
-    # Path dos dataframes
-    files = glob.glob(r"C:\Users\Felipe Sobral\Downloads\Dfs 2021\*.csv")
+        # Path dos dataframes
+        files = glob.glob(r"C:\Users\Felipe Sobral\Downloads\Dfs 2021\*.csv")
 
-    i = 1
-    for f in files:
-        df = pd.read_csv(f, sep=';', encoding='latin-1', on_bad_lines='skip')
-        df1 = pd.read_csv(f, sep=';', encoding='latin-1', skiprows=[0, 1, 2, 3, 4, 5, 6, 7], on_bad_lines='skip')
+        i = 1
+        for f in files:
+            df = pd.read_csv(f, sep=';', encoding='latin-1', on_bad_lines='skip')
+            df1 = pd.read_csv(f, sep=';', encoding='latin-1', skiprows=[0, 1, 2, 3, 4, 5, 6, 7], on_bad_lines='skip')
 
-        # Criando colunas do cabeçalho vertical
-        df['CODIGO (WMO)'] = df.loc[2][1]
+            # Criando colunas do cabeçalho vertical
+            df['CODIGO (WMO)'] = df.loc[2][1]
 
-        # Dando merge nos dataframes
-        df = df1.assign(key=1).merge(df[['CODIGO (WMO)']].assign(key=1), on='key').drop('key', axis=1)
-        del df1
+            # Dando merge nos dataframes
+            df = df1.assign(key=1).merge(df[['CODIGO (WMO)']].assign(key=1), on='key').drop('key', axis=1)
+            del df1
 
-        # Tratamento dos dados e povoamento do banco de dados
-        povoar_banco(tratamento_dfs(df))
+            # Tratamento dos dados e povoamento do banco de dados
+            ConexaoBD.povoar_banco(CleaningData.tratamento_dfs(df))
 
-        print(i)
-        i += 1
+            print(i)
+            i += 1
 
+    @staticmethod
+    def leitura_dfs2022():
 
-def leitura_dfs2022():
+        # Path dos dataframes
+        files = glob.glob(r"C:\Users\Felipe Sobral\Downloads\Dfs 2022\*.csv")
 
-    # Path dos dataframes
-    files = glob.glob(r"C:\Users\Felipe Sobral\Downloads\Dfs 2022\*.csv")
+        i = 1
+        for f in files:
+            df = pd.read_csv(f, sep=';', encoding='latin-1', on_bad_lines='skip')
+            df1 = pd.read_csv(f, sep=';', encoding='latin-1', skiprows=[0, 1, 2, 3, 4, 5, 6, 7], on_bad_lines='skip')
 
-    i = 1
-    for f in files:
-        df = pd.read_csv(f, sep=';', encoding='latin-1', on_bad_lines='skip')
-        df1 = pd.read_csv(f, sep=';', encoding='latin-1', skiprows=[0, 1, 2, 3, 4, 5, 6, 7], on_bad_lines='skip')
+            # Criando colunas do cabeçalho vertical
+            df['CODIGO (WMO)'] = df.loc[2][1]
 
-        # Criando colunas do cabeçalho vertical
-        df['CODIGO (WMO)'] = df.loc[2][1]
+            # Dando merge nos dataframes
+            df = df1.assign(key=1).merge(df[['CODIGO (WMO)']].assign(key=1), on='key').drop('key', axis=1)
+            del df1
 
-        # Dando merge nos dataframes
-        df = df1.assign(key=1).merge(df[['CODIGO (WMO)']].assign(key=1), on='key').drop('key', axis=1)
-        del df1
+            # Tratamento dos dados e povoamento do banco de dados
+            ConexaoBD.povoar_banco(CleaningData.tratamento_dfs(df))
 
-        # Tratamento dos dados e povoamento do banco de dados
-        povoar_banco(tratamento_dfs(df))
+            print(i)
+            i += 1
 
-        print(i)
-        i += 1
+    @staticmethod
+    def leitura_cabecalho2020():
 
+        # Path dos dataframes
+        files = glob.glob(r"C:\Users\Felipe Sobral\Downloads\Dfs 2020\*.csv")
 
-def leitura_cabecalho2020():
+        i = 1
+        for f in files:
+            df = pd.read_csv(f, sep=';', encoding='latin-1', on_bad_lines='skip', header=None)
 
-    # Path dos dataframes
-    files = glob.glob(r"C:\Users\Felipe Sobral\Downloads\Dfs 2020\*.csv")
+            # Tratamento dos dados e povoamento do banco de dados
+            ConexaoBD.estacao_banco(Cabecalho.tratamento_cabecalho(df))
 
-    i = 1
-    for f in files:
-        df = pd.read_csv(f, sep=';', encoding='latin-1', on_bad_lines='skip', header=None)
+            print(i)
+            i += 1
 
-        # Tratamento dos dados e povoamento do banco de dados
-        estacao_banco(tratamento_cabecalho(df))
+    @staticmethod
+    def leitura_cabecalho2021():
 
-        print(i)
-        i += 1
+        # Path dos dataframes
+        files = glob.glob(r"C:\Users\Felipe Sobral\Downloads\Dfs 2021\*.csv")
 
+        i = 1
+        for f in files:
+            df = pd.read_csv(f, sep=';', encoding='latin-1', on_bad_lines='skip', header=None)
 
-def leitura_cabecalho2021():
-
-    # Path dos dataframes
-    files = glob.glob(r"C:\Users\Felipe Sobral\Downloads\Dfs 2021\*.csv")
-
-    i = 1
-    for f in files:
-        df = pd.read_csv(f, sep=';', encoding='latin-1', on_bad_lines='skip', header=None)
-
-        # Tratamento dos dados e povoamento do banco de dados
-        estacao_banco(tratamento_cabecalho(df))
-
-
-        print(i)
-        i += 1
-
-
-def leitura_cabecalho2022():
-
-    # Path dos dataframes
-    files = glob.glob(r"C:\Users\Felipe Sobral\Downloads\Dfs 2022\*.csv")
-
-    i = 1
-    for f in files:
-        df = pd.read_csv(f, sep=';', encoding='latin-1', on_bad_lines='skip', header=None)
-
-        # Tratamento dos dados e povoamento do banco de dados
-        estacao_banco(tratamento_cabecalho(df))
+            # Tratamento dos dados e povoamento do banco de dados
+            ConexaoBD.estacao_banco(Cabecalho.tratamento_cabecalho(df))
 
 
-        print(i)
-        i += 1
+            print(i)
+            i += 1
+
+    @staticmethod
+    def leitura_cabecalho2022():
+
+        # Path dos dataframes
+        files = glob.glob(r"C:\Users\Felipe Sobral\Downloads\Dfs 2022\*.csv")
+
+        i = 1
+        for f in files:
+            df = pd.read_csv(f, sep=';', encoding='latin-1', on_bad_lines='skip', header=None)
+
+            # Tratamento dos dados e povoamento do banco de dados
+            ConexaoBD.estacao_banco(Cabecalho.tratamento_cabecalho(df))
+
+
+            print(i)
+            i += 1
 
 
 
