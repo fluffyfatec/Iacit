407b35f638b9277a34f75e4d3d869cb82777a7eb
Felipe Sobral <felipesobral_@hotmail.com>
2022-09-07 18:21:48 -0300

    Adicionando log ao código
    Logging para documentação de erros no código, gerado em arquivo TXT, retornando a datahora do erro e o 'raise error type'

--- CSVs/leituraDfs.py
+++ CSVs/leituraDfs.py
@@ -3,6 +3,7 @@ import glob
 from conexaoBD import ConexaoBD
 from cleaningData import CleaningData
 from cabeçalho import Cabecalho
+import logging
 
 
 class LeituraDfs:
@@ -10,125 +11,156 @@ class LeituraDfs:
     @staticmethod
     def leitura_dfs2020():
 
-        # Path dos dataframes
-        files = glob.glob(r"C:\Users\Felipe Sobral\Downloads\Dfs 2020\*.csv")
+        try:
+            # Path dos dataframes
+            files = glob.glob(r"C:\Users\Felipe Sobral\Downloads\Dfs 2020\*.csv")
 
-        i = 1
-        for f in files:
-            df = pd.read_csv(f, sep=';', encoding='latin-1', on_bad_lines='skip')
-            df1 = pd.read_csv(f, sep=';', encoding='latin-1', skiprows=[0, 1, 2, 3, 4, 5, 6, 7], on_bad_lines='skip')
+            i = 1
+            for f in files:
+                df = pd.read_csv(f, sep=';', encoding='latin-1', on_bad_lines='skip')
+                df1 = pd.read_csv(f, sep=';', encoding='latin-1', skiprows=[0, 1, 2, 3, 4, 5, 6, 7],
+                                  on_bad_lines='skip')
 
-            # Criando colunas do cabeçalho vertical
-            df['CODIGO (WMO)'] = df.loc[2][1]
+                # Criando colunas do cabeçalho vertical
+                df['CODIGO (WMO)'] = df.loc[2][1]
 
-            # Dando merge nos dataframes
-            df = df1.assign(key=1).merge(df[['CODIGO (WMO)']].assign(key=1), on='key').drop('key', axis=1)
-            del df1
+                # Dando merge nos dataframes
+                df = df1.assign(key=1).merge(df[['CODIGO (WMO)']].assign(key=1), on='key').drop('key', axis=1)
+                del df1
 
-            # Tratamento dos dados e povoamento do banco de dados
-            ConexaoBD.povoar_banco(CleaningData.tratamento_dfs(df))
+                # Tratamento dos dados e povoamento do banco de dados
+                ConexaoBD.povoar_banco(CleaningData.tratamento_dfs(df))
 
-            print(i)
-            i += 1
+                print(i)
+                i += 1
+        except:
+            logging.basicConfig(filename="log.txt", level=logging.DEBUG,
+                                format="%(asctime)s %(message)s", filemode="a")
+            logging.exception("- ERRO")
+            raise
 
     @staticmethod
     def leitura_dfs2021():
 
-        # Path dos dataframes
-        files = glob.glob(r"C:\Users\Felipe Sobral\Downloads\Dfs 2021\*.csv")
+        try:
+            # Path dos dataframes
+            files = glob.glob(r"C:\Users\Felipe Sobral\Downloads\Dfs 2021\*.csv")
 
-        i = 1
-        for f in files:
-            df = pd.read_csv(f, sep=';', encoding='latin-1', on_bad_lines='skip')
-            df1 = pd.read_csv(f, sep=';', encoding='latin-1', skiprows=[0, 1, 2, 3, 4, 5, 6, 7], on_bad_lines='skip')
+            i = 1
+            for f in files:
+                df = pd.read_csv(f, sep=';', encoding='latin-1', on_bad_lines='skip')
+                df1 = pd.read_csv(f, sep=';', encoding='latin-1', skiprows=[0, 1, 2, 3, 4, 5, 6, 7], on_bad_lines='skip')
 
-            # Criando colunas do cabeçalho vertical
-            df['CODIGO (WMO)'] = df.loc[2][1]
+                # Criando colunas do cabeçalho vertical
+                df['CODIGO (WMO)'] = df.loc[2][1]
 
-            # Dando merge nos dataframes
-            df = df1.assign(key=1).merge(df[['CODIGO (WMO)']].assign(key=1), on='key').drop('key', axis=1)
-            del df1
+                # Dando merge nos dataframes
+                df = df1.assign(key=1).merge(df[['CODIGO (WMO)']].assign(key=1), on='key').drop('key', axis=1)
+                del df1
 
-            # Tratamento dos dados e povoamento do banco de dados
-            ConexaoBD.povoar_banco(CleaningData.tratamento_dfs(df))
+                # Tratamento dos dados e povoamento do banco de dados
+                ConexaoBD.povoar_banco(CleaningData.tratamento_dfs(df))
 
-            print(i)
-            i += 1
+                print(i)
+                i += 1
+        except:
+            logging.basicConfig(filename="log.txt", level=logging.DEBUG,
+                                format="%(asctime)s %(message)s", filemode="a")
+            logging.exception("- ERRO")
+            raise
 
     @staticmethod
     def leitura_dfs2022():
 
-        # Path dos dataframes
-        files = glob.glob(r"C:\Users\Felipe Sobral\Downloads\Dfs 2022\*.csv")
+        try:
+            # Path dos dataframes
+            files = glob.glob(r"C:\Users\Felipe Sobral\Downloads\Dfs 2022\*.csv")
 
-        i = 1
-        for f in files:
-            df = pd.read_csv(f, sep=';', encoding='latin-1', on_bad_lines='skip')
-            df1 = pd.read_csv(f, sep=';', encoding='latin-1', skiprows=[0, 1, 2, 3, 4, 5, 6, 7], on_bad_lines='skip')
+            i = 1
+            for f in files:
+                df = pd.read_csv(f, sep=';', encoding='latin-1', on_bad_lines='skip')
+                df1 = pd.read_csv(f, sep=';', encoding='latin-1', skiprows=[0, 1, 2, 3, 4, 5, 6, 7], on_bad_lines='skip')
 
-            # Criando colunas do cabeçalho vertical
-            df['CODIGO (WMO)'] = df.loc[2][1]
+                # Criando colunas do cabeçalho vertical
+                df['CODIGO (WMO)'] = df.loc[2][1]
 
-            # Dando merge nos dataframes
-            df = df1.assign(key=1).merge(df[['CODIGO (WMO)']].assign(key=1), on='key').drop('key', axis=1)
-            del df1
+                # Dando merge nos dataframes
+                df = df1.assign(key=1).merge(df[['CODIGO (WMO)']].assign(key=1), on='key').drop('key', axis=1)
+                del df1
 
-            # Tratamento dos dados e povoamento do banco de dados
-            ConexaoBD.povoar_banco(CleaningData.tratamento_dfs(df))
+                # Tratamento dos dados e povoamento do banco de dados
+                ConexaoBD.povoar_banco(CleaningData.tratamento_dfs(df))
 
-            print(i)
-            i += 1
+                print(i)
+                i += 1
+        except:
+            logging.basicConfig(filename="log.txt", level=logging.DEBUG,
+                                format="%(asctime)s %(message)s", filemode="a")
+            logging.exception("- ERRO")
+            raise
 
     @staticmethod
     def leitura_cabecalho2020():
 
-        # Path dos dataframes
-        files = glob.glob(r"C:\Users\Felipe Sobral\Downloads\Dfs 2020\*.csv")
+        try:
+            # Path dos dataframes
+            files = glob.glob(r"C:\Users\Felipe Sobral\Downloads\Dfs 2020\*.csv")
 
-        i = 1
-        for f in files:
-            df = pd.read_csv(f, sep=';', encoding='latin-1', on_bad_lines='skip', header=None)
+            i = 1
+            for f in files:
+                df = pd.read_csv(f, sep=';', encoding='latin-1', on_bad_lines='skip', header=None)
 
-            # Tratamento dos dados e povoamento do banco de dados
-            ConexaoBD.estacao_banco(Cabecalho.tratamento_cabecalho(df))
+                # Tratamento dos dados e povoamento do banco de dados
+                ConexaoBD.estacao_banco(Cabecalho.tratamento_cabecalho(df))
 
-            print(i)
-            i += 1
+                print(i)
+                i += 1
+        except:
+            logging.basicConfig(filename="log.txt", level=logging.DEBUG,
+                                format="%(asctime)s %(message)s", filemode="a")
+            logging.exception("- ERRO")
+            raise
 
     @staticmethod
     def leitura_cabecalho2021():
 
-        # Path dos dataframes
-        files = glob.glob(r"C:\Users\Felipe Sobral\Downloads\Dfs 2021\*.csv")
+        try:
+            # Path dos dataframes
+            files = glob.glob(r"C:\Users\Felipe Sobral\Downloads\Dfs 2021\*.csv")
 
-        i = 1
-        for f in files:
-            df = pd.read_csv(f, sep=';', encoding='latin-1', on_bad_lines='skip', header=None)
+            i = 1
+            for f in files:
+                df = pd.read_csv(f, sep=';', encoding='latin-1', on_bad_lines='skip', header=None)
 
-            # Tratamento dos dados e povoamento do banco de dados
-            ConexaoBD.estacao_banco(Cabecalho.tratamento_cabecalho(df))
+                # Tratamento dos dados e povoamento do banco de dados
+                ConexaoBD.estacao_banco(Cabecalho.tratamento_cabecalho(df))
 
-
-            print(i)
-            i += 1
+                print(i)
+                i += 1
+        except:
+            logging.basicConfig(filename="log.txt", level=logging.DEBUG,
+                                format="%(asctime)s %(message)s", filemode="a")
+            logging.exception("- ERRO")
+            raise
 
     @staticmethod
     def leitura_cabecalho2022():
 
-        # Path dos dataframes
-        files = glob.glob(r"C:\Users\Felipe Sobral\Downloads\Dfs 2022\*.csv")
+        try:
+            # Path dos dataframes
+            files = glob.glob(r"C:\Users\Felipe Sobral\Downloads\Dfs 2022\*.csv")
 
-        i = 1
-        for f in files:
-            df = pd.read_csv(f, sep=';', encoding='latin-1', on_bad_lines='skip', header=None)
+            i = 1
+            for f in files:
+                df = pd.read_csv(f, sep=';', encoding='latin-1', on_bad_lines='skip', header=None)
 
-            # Tratamento dos dados e povoamento do banco de dados
-            ConexaoBD.estacao_banco(Cabecalho.tratamento_cabecalho(df))
+                # Tratamento dos dados e povoamento do banco de dados
+                ConexaoBD.estacao_banco(Cabecalho.tratamento_cabecalho(df))
 
-
-            print(i)
-            i += 1
-
-
-
-
+                print(i)
+                i += 1
+        except:
+            logging.basicConfig(filename="log.txt", level=logging.DEBUG,
+                                format="%(asctime)s %(message)s", filemode="a")
+            logging.exception("- ERRO")
+            raise
