dc2f68e66fdcbe1d04dfacaf4d54e75b71fc2f05
tcamillo4-ctrlz <tcamillo.tecnico@gmail.com>
2022-09-10 12:57:03 -0300

    Update Filtros
    Adicionadas funções para os campos de filtros.

---
+++ BACKEND.IACIT/src/main/resources/static/js/filtro.js
@@ -0,0 +1,17 @@
+function filtroEstado(){
+    var opcestado = document.getElementById('selectestado').value;
+    console.log(opcestado);
+}
+
+function filtroEstacao(){
+    var opcestacao = document.getElementById('selectestacao').value;
+    console.log(opcestacao);
+}
+
+
+
+function filtrar(){
+    filtroEstado();
+    filtroEstacao();
+
+}
