4190625c8c329747dd209c64259da7b7cceead82
Patrick Souza <patrickgabriel103f@gmail.com>
2022-09-11 13:53:24 -0300

    Adução do GITIGNORE
    GITIGNORE para evitar subir arquivos .csv e .zip para o git.

--- CSVs/automacao.py
+++ CSVs/automacao.py
@@ -1,50 +1,57 @@
-def auto_run():   
-    import os
-    import requests
-    from datetime import date
-    import zipfile
-     # Criando a variavel do ano atual para o url
-    date_td = date.today()
-    year_td = date_td.year
-    
-    # Função para baixar os csv's
-    def download_df(url, endereco):
-        # try - except para criar um novo diretorio para os arquivos do ano
-        try:
-                os.mkdir('DF/{}'.format(year_td))
-                print("Diretorio {} criado".format(year_td))
-        except:
-            print("O diretorio {} já existe".format(year_td))
-        status = requests.get(url)
-        if status.status_code == requests.codes.OK:
-            with open(endereco, 'wb') as novo_arquivo:
-                novo_arquivo.write(status.content)
-            print("Donwload finalizado. Salvo em: {}".format(endereco))
-           
+class Automacao: 
+    def auto_run():   
+        import os
+        import requests #pip install requests
+        from datetime import date
+        import zipfile
+        import shutil #pip install shutil
+
+        # Criando a variavel do ano atual para o url
+        date_td = date.today()
+        year_td = date_td.year
+
+        # Para cada ano de 2020 até o ano atual executar o codigo
+        for i in range(2020, year_td + 1):
+
+        # Função para baixar os csv's
+            def download_df(url, endereco):
             
-        else:
-            status.raise_for_status()
+                # try - except para criar um novo diretorio para os arquivos do ano
+                try:
+                        os.mkdir("DF/{}".format(i))
+                except:
+                    shutil.rmtree(f"DF/{i}", ignore_errors=False, onerror=None)
+                    os.mkdir("DF/{}".format(i))
+                status = requests.get(url)
+                if status.status_code == requests.codes.OK:
+                    with open(endereco, "wb") as novo_arquivo:
+                        novo_arquivo.write(status.content)
+                
+                else:
+                    status.raise_for_status()
 
-    if __name__ == "__main__":
-        
-        BASE_URL = 'https://portal.inmet.gov.br/uploads/dadoshistoricos/{}.zip'
-        nome_arquivo = os.path.join("DF","{}".format(year_td),'{}.zip'.format(year_td))
-        
-        download_df(BASE_URL.format(year_td), nome_arquivo)
-        
 
-        # Função para extrair os arquivos
-        def extract ():
+            if __name__ == "__main__":
+            
+                BASE_URL = "https://portal.inmet.gov.br/uploads/dadoshistoricos/{}.zip"
+                nome_arquivo = os.path.join("DF","{}.zip".format(i))
+                
+                download_df(BASE_URL.format(i), nome_arquivo)
+                
 
-                zip_ref = zipfile.ZipFile(f'DF/{year_td}/{year_td}.zip', 'r')
-                reference = ("DF/{}".format(year_td))
-                zip_ref.extractall(reference)
-                zip_ref.close()
-                print(f"Arquivos extraidos em {reference}")
+                # Função para extrair os arquivos
+                def extract ():
+            
+                    zip_ref = zipfile.ZipFile("DF/{}.zip".format(i), "r")
+                    reference = ("DF/{}".format(i))
+                    zip_ref.extractall(reference)
+                    zip_ref.close()
+                    os.remove("DF/{}.zip".format(i))
+                    print("cabo api")
 
-        extract()
+                extract()
 
-auto_run()
+    auto_run()
 
 
 
