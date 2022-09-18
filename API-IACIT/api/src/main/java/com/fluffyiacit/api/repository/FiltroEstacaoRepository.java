a695e71a74a911195ae94441bb676ad0fbac566e
AldrikAlvaro <51426536+Aldrik-Alvaro@users.noreply.github.com>
2022-09-18 19:52:18 -0300

    Adição de legenda dos dados
    Realizado a implementação das legendas dos dados no FRONT END

--- API-IACIT/api/src/main/java/com/fluffyiacit/api/repository/FiltroEstacaoRepository.java
+++ API-IACIT/api/src/main/java/com/fluffyiacit/api/repository/FiltroEstacaoRepository.java
@@ -9,6 +9,6 @@ import org.springframework.data.repository.query.Param;
 import com.fluffyiacit.api.modal.ViewFiltroEstacao;
 
 public interface FiltroEstacaoRepository extends JpaRepository<ViewFiltroEstacao, String> {
-	@Query ("SELECT v FROM view_filtro_estacao v WHERE v.estacao_estado = :estacao_estado")
+	@Query ("SELECT v FROM view_filtro_estacao v WHERE v.estacao_estado = :estacao_estado ORDER BY estacao_nome")
 	public List<ViewFiltroEstacao> listar(@Param("estacao_estado") String estacao_estado);
 }
