f28822c9244e7d7ae3f66dcbc6d903dd78bf11c6
Fluffy Integrative Project - FATEC <88806221+fluffyfatec@users.noreply.github.com>
2022-09-22 19:33:47 -0300

    Correcao Controller variaveis

--- API-IACIT/api/src/main/java/com/fluffyiacit/api/controller/HomeController.java
+++ API-IACIT/api/src/main/java/com/fluffyiacit/api/controller/HomeController.java
@@ -3,6 +3,7 @@ package com.fluffyiacit.api.controller;
 import java.sql.Timestamp;
 import java.util.List;
 
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -10,9 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
-import DTO.FiltroDTO;
-
-
+import com.fluffyiacit.api.modal.FiltroModal;
 import com.fluffyiacit.api.modal.ViewFiltroEstacao;
 import com.fluffyiacit.api.modal.ViewPrecipitacaoModal;
 import com.fluffyiacit.api.modal.ViewPressaoAtmModal;
@@ -28,11 +27,14 @@ import com.fluffyiacit.api.repository.TemperaturaRepository;
 import com.fluffyiacit.api.repository.UmidadeRepository;
 import com.fluffyiacit.api.repository.VentoRepository;
 
+
 @Controller
 @RequestMapping
 public class HomeController {
 
 	// REPOSITORY
+
+
 	@Autowired(required = true)
 	private PrecipitacaoRepository precipitacao;
 
@@ -53,19 +55,19 @@ public class HomeController {
 
 	@Autowired(required = true)
 	private FiltroEstacaoRepository filtroestacaorepository;
-	//
 
 	// ENTRAR PAGINA INDEX
+	
 	@RequestMapping(value = { "" }, method = RequestMethod.GET)
 	public ModelAndView PaginaIndex() {
 		ModelAndView modelAndView = new ModelAndView();
 
-		FiltroDTO filtromodal = new FiltroDTO();
+		FiltroModal filtromodal = new FiltroModal();
 
-		filtromodal.setEstacao_nome("");
-		filtromodal.setEstacao_estado("");
-		filtromodal.setCod_wmo("");
-		filtromodal.setDatahora_captacao("2022-06-28 10:00:00");
+		filtromodal.setEstacaoNome("");
+		filtromodal.setEstacaoEstado("");
+		filtromodal.setCodWmo("");
+		filtromodal.setDatahoraCaptacao("2022-06-28 10:00:00");
 		modelAndView.addObject("filtro", filtromodal);
 
 		// Lista Precipitacao
@@ -82,19 +84,19 @@ public class HomeController {
 		List<ViewPressaoAtmModal> teste_atm = pressao.listar("SP", "SAO PAULO - INTERLAGOS",
 				Timestamp.valueOf("2022-06-28 10:00:00"));
 		for (ViewPressaoAtmModal objviewPressao : teste_atm) {
-			if (objviewPressao.getPressao_atm_estacao() == null) {
-				objviewPressao.setPressao_atm_estacao("N/A");
+			if (objviewPressao.getPressaoAtmEstacao() == null) {
+				objviewPressao.setPressaoAtmEstacao("N/A");
 			}
 		}
 		for (ViewPressaoAtmModal objviewPressaoMax : teste_atm) {
-			if (objviewPressaoMax.getPressao_atm_max() == null) {
-				objviewPressaoMax.setPressao_atm_max("N/A");
+			if (objviewPressaoMax.getPressaoAtmMax() == null) {
+				objviewPressaoMax.setPressaoAtmMax("N/A");
 
 			}
 		}
 		for (ViewPressaoAtmModal objviewPressaoMin : teste_atm) {
-			if (objviewPressaoMin.getPressao_atm_min() == null) {
-				objviewPressaoMin.setPressao_atm_min("N/A");
+			if (objviewPressaoMin.getPressaoAtmMin() == null) {
+				objviewPressaoMin.setPressaoAtmMin("N/A");
 
 			}
 		}
@@ -104,8 +106,8 @@ public class HomeController {
 		List<ViewRadiacaoglobalModal> teste_radiacaoglobal = radiacao.listar("SP", "SAO PAULO - INTERLAGOS",
 				Timestamp.valueOf("2022-06-28 10:00:00"));
 		for (ViewRadiacaoglobalModal objviewRadiacao : teste_radiacaoglobal) {
-			if (objviewRadiacao.getRadiacao_global() == null) {
-				objviewRadiacao.setRadiacao_global("N/A");
+			if (objviewRadiacao.getRadiacaoGlobal() == null) {
+				objviewRadiacao.setRadiacaoGlobal("N/A");
 			}
 		}
 		modelAndView.addObject("teste_radiacaoglobal", teste_radiacaoglobal);
@@ -114,32 +116,32 @@ public class HomeController {
 		List<ViewTemperaturaModal> teste_temperatura = temperatura.listar("SP", "SAO PAULO - INTERLAGOS",
 				Timestamp.valueOf("2022-06-28 10:00:00"));
 		for (ViewTemperaturaModal objviewTemperatura : teste_temperatura) {
-			if (objviewTemperatura.getTemperatura_ar() == null) {
-				objviewTemperatura.setTemperatura_ar("N/A");
+			if (objviewTemperatura.getTemperaturaAr() == null) {
+				objviewTemperatura.setTemperaturaAr("N/A");
 			}
 			for (ViewTemperaturaModal objviewTemperaturaMax : teste_temperatura) {
-				if (objviewTemperaturaMax.getTemperatura_max() == null) {
-					objviewTemperaturaMax.setTemperatura_max("N/A");
+				if (objviewTemperaturaMax.getTemperaturaMax() == null) {
+					objviewTemperaturaMax.setTemperaturaMax("N/A");
 				}
 			}
 			for (ViewTemperaturaModal objviewTemperaturaMin : teste_temperatura) {
-				if (objviewTemperaturaMin.getTemperatura_min() == null) {
-					objviewTemperaturaMin.setTemperatura_min("N/A");
+				if (objviewTemperaturaMin.getTemperaturaMin() == null) {
+					objviewTemperaturaMin.setTemperaturaMin("N/A");
 				}
 			}
 			for (ViewTemperaturaModal objviewTemperaturaOrvalhoMax : teste_temperatura) {
-				if (objviewTemperaturaOrvalhoMax.getTemperatura_orvalho_max() == null) {
-					objviewTemperaturaOrvalhoMax.setTemperatura_orvalho_max("N/A");
+				if (objviewTemperaturaOrvalhoMax.getTemperaturaOrvalhoMax() == null) {
+					objviewTemperaturaOrvalhoMax.setTemperaturaOrvalhoMax("N/A");
 				}
 			}
 			for (ViewTemperaturaModal objviewTemperaturaOrvalhoMin : teste_temperatura) {
-				if (objviewTemperaturaOrvalhoMin.getTemperatura_orvalho_min() == null) {
-					objviewTemperaturaOrvalhoMin.setTemperatura_orvalho_min("N/A");
+				if (objviewTemperaturaOrvalhoMin.getTemperaturaOrvalhoMin() == null) {
+					objviewTemperaturaOrvalhoMin.setTemperaturaOrvalhoMin("N/A");
 				}
 			}
 			for (ViewTemperaturaModal objviewTemperaturaPontoOrvalho : teste_temperatura) {
-				if (objviewTemperaturaPontoOrvalho.getTemperatura_ponto_orvalho() == null) {
-					objviewTemperaturaPontoOrvalho.setTemperatura_ponto_orvalho("N/A");
+				if (objviewTemperaturaPontoOrvalho.getTemperaturaPontoOrvalho() == null) {
+					objviewTemperaturaPontoOrvalho.setTemperaturaPontoOrvalho("N/A");
 				}
 			}
 
@@ -150,18 +152,18 @@ public class HomeController {
 		List<ViewUmidadeModal> teste_umidade = umidade.listar("SP", "SAO PAULO - INTERLAGOS",
 				Timestamp.valueOf("2022-06-28 10:00:00"));
 		for (ViewUmidadeModal objviewUmidade : teste_umidade) {
-			if (objviewUmidade.getUmidade_relativa_ar() == null) {
-				objviewUmidade.setUmidade_relativa_ar("N/A");
+			if (objviewUmidade.getUmidadeRelativaAr() == null) {
+				objviewUmidade.setUmidadeRelativaAr("N/A");
 			}
 		}
 		for (ViewUmidadeModal objviewUmidadeRelativaMax : teste_umidade) {
-			if (objviewUmidadeRelativaMax.getUmidade_relativa_max() == null) {
-				objviewUmidadeRelativaMax.setUmidade_relativa_max("N/A");
+			if (objviewUmidadeRelativaMax.getUmidadeRelativaMax() == null) {
+				objviewUmidadeRelativaMax.setUmidadeRelativaMax("N/A");
 			}
 		}
 		for (ViewUmidadeModal objviewUmidadeRelativaMin : teste_umidade) {
-			if (objviewUmidadeRelativaMin.getUmidade_relativa_min() == null) {
-				objviewUmidadeRelativaMin.setUmidade_relativa_min("N/A");
+			if (objviewUmidadeRelativaMin.getUmidadeRelativaMin() == null) {
+				objviewUmidadeRelativaMin.setUmidadeRelativaMin("N/A");
 			}
 		}
 		modelAndView.addObject("teste_umidade", teste_umidade);
@@ -170,18 +172,18 @@ public class HomeController {
 		List<ViewVentoModal> teste_vento = vento.listar("SP", "SAO PAULO - INTERLAGOS",
 				Timestamp.valueOf("2022-06-28 10:00:00"));
 		for (ViewVentoModal objviewVento : teste_vento) {
-			if (objviewVento.getVento_direcao_horario() == null) {
-				objviewVento.setVento_direcao_horario("N/A");
+			if (objviewVento.getVentoDirecaoHorario() == null) {
+				objviewVento.setVentoDirecaoHorario("N/A");
 			}
 		}
 		for (ViewVentoModal objviewVentoMax : teste_vento) {
-			if (objviewVentoMax.getVento_rajada_max() == null) {
-				objviewVentoMax.setVento_rajada_max("N/A");
+			if (objviewVentoMax.getVentoRajadaMax() == null) {
+				objviewVentoMax.setVentoRajadaMax("N/A");
 			}
 		}
 		for (ViewVentoModal objviewVentoVelocidade : teste_vento) {
-			if (objviewVentoVelocidade.getVento_velocidade() == null) {
-				objviewVentoVelocidade.setVento_velocidade("N/A");
+			if (objviewVentoVelocidade.getVentoVelocidade() == null) {
+				objviewVentoVelocidade.setVentoVelocidade("N/A");
 			}
 		}
 		modelAndView.addObject("teste_vento", teste_vento);
@@ -194,13 +196,13 @@ public class HomeController {
 
 	// ENTRAR PAGINA INDEX FILTRADA
 	@RequestMapping(value = { "/index" }, method = RequestMethod.POST)
-	public ModelAndView PaginaIndexFiltrada(FiltroDTO filtromodal) {
+	public ModelAndView PaginaIndexFiltrada(FiltroModal filtromodal) {
 		ModelAndView modelAndView = new ModelAndView();
 		// System.out.println("1:" + filtromodal.getEstacao_nome());
 
 		// Lista Precipitacao
-		List<ViewPrecipitacaoModal> teste_precipitacao = precipitacao.listar(filtromodal.getEstacao_nome(),
-				filtromodal.getEstacao_estado(), Timestamp.valueOf(filtromodal.getDatahora_captacao()));
+		List<ViewPrecipitacaoModal> teste_precipitacao = precipitacao.listar(filtromodal.getEstacaoNome(),
+				filtromodal.getEstacaoEstado(), Timestamp.valueOf(filtromodal.getDatahoraCaptacao()));
 		for (ViewPrecipitacaoModal objviewPrecipitacao : teste_precipitacao) {
 			if (objviewPrecipitacao.getPrecipitacaototal() == null) {
 				objviewPrecipitacao.setPrecipitacaototal("N/A");
@@ -209,128 +211,127 @@ public class HomeController {
 		modelAndView.addObject("teste_precipitacao", teste_precipitacao);
 
 		// Lista Pressao
-		List<ViewPressaoAtmModal> teste_atm = pressao.listar(filtromodal.getEstacao_nome(),
-				filtromodal.getEstacao_estado(), Timestamp.valueOf(filtromodal.getDatahora_captacao()));
+		List<ViewPressaoAtmModal> teste_atm = pressao.listar(filtromodal.getEstacaoNome(),
+				filtromodal.getEstacaoEstado(), Timestamp.valueOf(filtromodal.getDatahoraCaptacao()));
 		for (ViewPressaoAtmModal objviewPressao : teste_atm) {
-			if (objviewPressao.getPressao_atm_estacao() == null) {
-				objviewPressao.setPressao_atm_estacao("N/A");
+			if (objviewPressao.getPressaoAtmEstacao() == null) {
+				objviewPressao.setPressaoAtmEstacao("N/A");
 			}
 		}
 		for (ViewPressaoAtmModal objviewPressaoMax : teste_atm) {
-			if (objviewPressaoMax.getPressao_atm_max() == null) {
-				objviewPressaoMax.setPressao_atm_max("N/A");
+			if (objviewPressaoMax.getPressaoAtmMax() == null) {
+				objviewPressaoMax.setPressaoAtmMax("N/A");
 
 			}
 		}
 		for (ViewPressaoAtmModal objviewPressaoMin : teste_atm) {
-			if (objviewPressaoMin.getPressao_atm_min() == null) {
-				objviewPressaoMin.setPressao_atm_min("N/A");
+			if (objviewPressaoMin.getPressaoAtmMin() == null) {
+				objviewPressaoMin.setPressaoAtmMin("N/A");
 
 			}
 		}
 		modelAndView.addObject("teste_atm", teste_atm);
 
 		// Lista Radiacao
-		List<ViewRadiacaoglobalModal> teste_radiacaoglobal = radiacao.listar(filtromodal.getEstacao_nome(),
-				filtromodal.getEstacao_estado(), Timestamp.valueOf(filtromodal.getDatahora_captacao()));
+		List<ViewRadiacaoglobalModal> teste_radiacaoglobal = radiacao.listar(filtromodal.getEstacaoNome(),
+				filtromodal.getEstacaoEstado(), Timestamp.valueOf(filtromodal.getDatahoraCaptacao()));
 		for (ViewRadiacaoglobalModal objviewRadiacao : teste_radiacaoglobal) {
-			if (objviewRadiacao.getRadiacao_global() == null) {
-				objviewRadiacao.setRadiacao_global("N/A");
+			if (objviewRadiacao.getRadiacaoGlobal() == null) {
+				objviewRadiacao.setRadiacaoGlobal("N/A");
 			}
 		}
 		modelAndView.addObject("teste_radiacaoglobal", teste_radiacaoglobal);
 
 		// Lista Temperatura
-		List<ViewTemperaturaModal> teste_temperatura = temperatura.listar(filtromodal.getEstacao_nome(),
-				filtromodal.getEstacao_estado(), Timestamp.valueOf(filtromodal.getDatahora_captacao()));
+		List<ViewTemperaturaModal> teste_temperatura = temperatura.listar(filtromodal.getEstacaoNome(),
+				filtromodal.getEstacaoEstado(), Timestamp.valueOf(filtromodal.getDatahoraCaptacao()));
 		for (ViewTemperaturaModal objviewTemperatura : teste_temperatura) {
-			if (objviewTemperatura.getTemperatura_ar() == null) {
-				objviewTemperatura.setTemperatura_ar("N/A");
+			if (objviewTemperatura.getTemperaturaAr() == null) {
+				objviewTemperatura.setTemperaturaAr("N/A");
 			}
 			for (ViewTemperaturaModal objviewTemperaturaMax : teste_temperatura) {
-				if (objviewTemperaturaMax.getTemperatura_max() == null) {
-					objviewTemperaturaMax.setTemperatura_max("N/A");
+				if (objviewTemperaturaMax.getTemperaturaMax() == null) {
+					objviewTemperaturaMax.setTemperaturaMax("N/A");
 				}
 			}
 			for (ViewTemperaturaModal objviewTemperaturaMin : teste_temperatura) {
-				if (objviewTemperaturaMin.getTemperatura_min() == null) {
-					objviewTemperaturaMin.setTemperatura_min("N/A");
+				if (objviewTemperaturaMin.getTemperaturaMin() == null) {
+					objviewTemperaturaMin.setTemperaturaMin("N/A");
 				}
 			}
 			for (ViewTemperaturaModal objviewTemperaturaOrvalhoMax : teste_temperatura) {
-				if (objviewTemperaturaOrvalhoMax.getTemperatura_orvalho_max() == null) {
-					objviewTemperaturaOrvalhoMax.setTemperatura_orvalho_max("N/A");
+				if (objviewTemperaturaOrvalhoMax.getTemperaturaOrvalhoMax() == null) {
+					objviewTemperaturaOrvalhoMax.setTemperaturaOrvalhoMax("N/A");
 				}
 			}
 			for (ViewTemperaturaModal objviewTemperaturaOrvalhoMin : teste_temperatura) {
-				if (objviewTemperaturaOrvalhoMin.getTemperatura_orvalho_min() == null) {
-					objviewTemperaturaOrvalhoMin.setTemperatura_orvalho_min("N/A");
+				if (objviewTemperaturaOrvalhoMin.getTemperaturaOrvalhoMin() == null) {
+					objviewTemperaturaOrvalhoMin.setTemperaturaOrvalhoMin("N/A");
 				}
 			}
 			for (ViewTemperaturaModal objviewTemperaturaPontoOrvalho : teste_temperatura) {
-				if (objviewTemperaturaPontoOrvalho.getTemperatura_ponto_orvalho() == null) {
-					objviewTemperaturaPontoOrvalho.setTemperatura_ponto_orvalho("N/A");
+				if (objviewTemperaturaPontoOrvalho.getTemperaturaPontoOrvalho() == null) {
+					objviewTemperaturaPontoOrvalho.setTemperaturaPontoOrvalho("N/A");
 				}
 
 			}
 
-			//System.out.println("1:" + objviewTemperatura.getTemperatura_max());
-			//System.out.println("1:" + objviewTemperatura.getTemperatura_orvalho_max());
-			//System.out.println("2:" + objviewTemperatura.getTemperatura_orvalho_min());
-			//System.out.println("3:" + objviewTemperatura.getTemperatura_ponto_orvalho());
+			// System.out.println("1:" + objviewTemperatura.getTemperatura_max());
+			// System.out.println("1:" + objviewTemperatura.getTemperatura_orvalho_max());
+			// System.out.println("2:" + objviewTemperatura.getTemperatura_orvalho_min());
+			// System.out.println("3:" + objviewTemperatura.getTemperatura_ponto_orvalho());
 		}
 
 		modelAndView.addObject("teste_temperatura", teste_temperatura);
 
 		// Lista Umidade
-		List<ViewUmidadeModal> teste_umidade = umidade.listar(filtromodal.getEstacao_nome(),
-				filtromodal.getEstacao_estado(), Timestamp.valueOf(filtromodal.getDatahora_captacao()));
+		List<ViewUmidadeModal> teste_umidade = umidade.listar(filtromodal.getEstacaoNome(),
+				filtromodal.getEstacaoEstado(), Timestamp.valueOf(filtromodal.getDatahoraCaptacao()));
 		for (ViewUmidadeModal objviewUmidade : teste_umidade) {
-			if (objviewUmidade.getUmidade_relativa_ar() == null) {
-				objviewUmidade.setUmidade_relativa_ar("N/A");
+			if (objviewUmidade.getUmidadeRelativaAr() == null) {
+				objviewUmidade.setUmidadeRelativaAr("N/A");
 			}
 		}
 		for (ViewUmidadeModal objviewUmidadeRelativaMax : teste_umidade) {
-			if (objviewUmidadeRelativaMax.getUmidade_relativa_max() == null) {
-				objviewUmidadeRelativaMax.setUmidade_relativa_max("N/A");
+			if (objviewUmidadeRelativaMax.getUmidadeRelativaMax() == null) {
+				objviewUmidadeRelativaMax.setUmidadeRelativaMax("N/A");
 			}
 		}
 		for (ViewUmidadeModal objviewUmidadeRelativaMin : teste_umidade) {
-			if (objviewUmidadeRelativaMin.getUmidade_relativa_min() == null) {
-				objviewUmidadeRelativaMin.setUmidade_relativa_min("N/A");
+			if (objviewUmidadeRelativaMin.getUmidadeRelativaMin() == null) {
+				objviewUmidadeRelativaMin.setUmidadeRelativaMin("N/A");
 			}
 		}
 		modelAndView.addObject("teste_umidade", teste_umidade);
 
 		// Lista Vento
-		List<ViewVentoModal> teste_vento = vento.listar(filtromodal.getEstacao_nome(), filtromodal.getEstacao_estado(),
-				Timestamp.valueOf(filtromodal.getDatahora_captacao()));
+		List<ViewVentoModal> teste_vento = vento.listar(filtromodal.getEstacaoNome(), filtromodal.getEstacaoEstado(),
+				Timestamp.valueOf(filtromodal.getDatahoraCaptacao()));
 		for (ViewVentoModal objviewVento : teste_vento) {
-			if (objviewVento.getVento_direcao_horario() == null) {
-				objviewVento.setVento_direcao_horario("N/A");
+			if (objviewVento.getVentoDirecaoHorario() == null) {
+				objviewVento.setVentoDirecaoHorario("N/A");
 			}
 		}
 		for (ViewVentoModal objviewVentoMax : teste_vento) {
-			if (objviewVentoMax.getVento_rajada_max() == null) {
-				objviewVentoMax.setVento_rajada_max("N/A");
+			if (objviewVentoMax.getVentoRajadaMax() == null) {
+				objviewVentoMax.setVentoRajadaMax("N/A");
 			}
 		}
 		for (ViewVentoModal objviewVentoVelocidade : teste_vento) {
-			if (objviewVentoVelocidade.getVento_velocidade() == null) {
-				objviewVentoVelocidade.setVento_velocidade("N/A");
+			if (objviewVentoVelocidade.getVentoVelocidade() == null) {
+				objviewVentoVelocidade.setVentoVelocidade("N/A");
 			}
 		}
 		modelAndView.addObject("teste_vento", teste_vento);
 
-		filtromodal.getEstacao_nome();
-		filtromodal.getEstacao_estado();
-		filtromodal.getDatahora_captacao();
+		filtromodal.getEstacaoNome();
+		filtromodal.getEstacaoEstado();
+		filtromodal.getDatahoraCaptacao();
 		modelAndView.addObject("filtro", filtromodal);
 
 		// INFORMANDO A PAGINA QUE SERA MOSTRADA
 		modelAndView.setViewName("index");
 		return modelAndView;
-		//
 	}
 
 	// FILTRO AJAX - ESTACAO
@@ -343,8 +344,9 @@ public class HomeController {
 
 		// INFORMANDO A PAGINA QUE SERA MOSTRADA
 		modelAndView.setViewName("AjaxFiltro");
+
 		return modelAndView;
-		//
+
 	}
 
 	// FILTRO AJAX - ESTACAO NULO
@@ -355,7 +357,6 @@ public class HomeController {
 		// INFORMANDO A PAGINA QUE SERA MOSTRADA
 		modelAndView.setViewName("AjaxFiltroNulo");
 		return modelAndView;
-		//
-	}
+}
 
 }
