168cb720fff3cc4f4aea98704d2124712da7ca57
AldrikAlvaro <51426536+Aldrik-Alvaro@users.noreply.github.com>
2022-10-07 02:43:43 -0300

    task 82 - correcoes
    realizada as devidas correcoes no codigo

--- API-IACIT/api/src/main/resources/static/js/filtro.js
+++ API-IACIT/api/src/main/resources/static/js/filtro.js
@@ -1,55 +1,78 @@
 
   document.getElementById("jsRegiao").onchange = function() {filtroRegiao()};
   
+  //COMBO BOX - REGIÃO
   function filtroRegiao(){
     var jsFiltroRegiao= document.getElementById("jsRegiao").value;
-    $('#jsrecebeRegiao').load('/ajax/filtro/' + jsFiltroRegiao);
-
-  }
-
-  function FiltroRegiaoEstado(){
-    var jsfiltroEstado = document.getElementById("jsfiltroEstadoRegiao");
-      var jsEstasdo = jsfiltroEstado.options[jsfiltroEstado.selectedIndex].text;
-    
-
-    if(jsEstasdo == ''){
+    if(jsFiltroRegiao == ''){
+      $('#jsrecebeRegiao').load('/filtro/ajax');
       $('#jv_classe').load('/filtro/ajax');
     }else{
-      $('#jv_classe').load('/filtro/ajax/' + jsEstasdo);
+      $('#jsrecebeRegiao').load('/filtro/ajax');
+      $('#jv_classe').load('/filtro/ajax');
+      $('#jsrecebeRegiao').load('/ajax/filtro/' + jsFiltroRegiao);
+    }
+  }
+
+  //COMBO BOX - ESTADO
+  function FiltroRegiaoEstado(){
+      var jsfiltroEstado = document.getElementById("jsfiltroEstadoRegiao");
+      var jsEstado = jsfiltroEstado.options[jsfiltroEstado.selectedIndex].text;
+      document.getElementById("jsRecebeEstado").value = jsEstado;
+      if(jsEstado == 'Escolha o Estado...'){
+        $('#jv_classe').load('/filtro/ajax');
+      }else{
+        $('#jv_classe').load('/filtro/ajax/' + jsEstado);
+    }
+  }
+
+  //COMBO BOX - ESTAÇÃO
+  function filtroEstacao() {  
+    var jsAjaxFiltro = document.getElementById("jsAjaxFiltro");
+    var opcaoTexto = jsAjaxFiltro.options[jsAjaxFiltro.selectedIndex].text;
+    //alert(opcaoTexto);
+    document.getElementById("jsRecebeEstacao").value = opcaoTexto;
   }
  
-}
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
+  //RECEBENDO DATA CAPTAÇÃO MINIMA
+  document.getElementById("jsEnviaDataMin").onchange = function() {filtroDataMin()};
+  function filtroDataMin() {
+        var jsEnviaDataMin = document.getElementById("jsEnviaDataMin").value;
+        var jsData = jsEnviaDataMin.substring(0, 10);
+        var jsHora = jsEnviaDataMin.substring(11);
+        var jsHora = jsHora.substring(0,2);
+        var jsHora = jsHora + ':00:00';
+        document.getElementById("jsRecebeDataMin").value = jsData + ' ' + jsHora;
+        validaData();
+  }
+
+    //RECEBENDO DATA CAPTAÇÃO MAXIMA
+    document.getElementById("jsEnviaDataMax").onchange = function() {filtroDataMax()};
+    function filtroDataMax() {
+          var jsEnviaDataMax = document.getElementById("jsEnviaDataMax").value;
+          var jsData = jsEnviaDataMax.substring(0, 10);
+          var jsHora = jsEnviaDataMax.substring(11);
+          var jsHora = jsHora.substring(0,2);
+          var jsHora = jsHora + ':00:00';
+          document.getElementById("jsRecebeDataMax").value = jsData + ' ' + jsHora;
+          validaData();
+    }
+  
+  //VALIDADOR DE DATAS
+  function validaData(){
+    var jsEnviaDataMax = document.getElementById("jsEnviaDataMax").value;
+    var jsEnviaDataMin = document.getElementById("jsEnviaDataMin").value;
+
+    if(jsEnviaDataMin > jsEnviaDataMax && jsEnviaDataMax != ''){
+      alert("Erro! data Maxima menor que data Minima");
+      document.getElementById("jsEnviaDataMin").value = '';
+      document.getElementById("jsEnviaDataMax").value = '';
+    }
+  }
+////////////////////////////////////////////////////////////////////////////////////////////////////////  
+//////////////////////////////////////////////////////////////////////////////////////////////////////// 
+//////////////////////////////////////////////////////////////////////////////////////////////////////// 
+
  //MENU LATERAL / DETALHES
   function jsMenuAjax(dado){
     //alert(dado);
@@ -63,7 +86,6 @@
     });
   }
 
-
   //ABRE A MODAL FILTRO
   function jsAbrirModalInfo(){
       $("#ModalInfo").modal({
@@ -71,71 +93,48 @@
     });
   }
 
-  // //EXECUTA O AJAX FILTRO
-  // function filtroAjax() {
-  //   var jsEstasdo = document.getElementById("jsEnviaEstado").value;
+////////////////////////////////////////////////////////////////////////////////////////////////////////  
+//////////////////////////////////////////////////////////////////////////////////////////////////////// 
+//////////////////////////////////////////////////////////////////////////////////////////////////////// 
+
+//TRATANDO DATA TOPO DA PAGINA
+function jsFiltroDashboard() {
+  dtMin();
+  dtMax();
+}
+
+//TRATANDO DATA MAXIMA - TOPO DA PAGINA
+function dtMax() {
+
+    var jsFiltroData = document.getElementById("jsRecebeDataMax").value;
     
-  //   if(jsEstasdo == ''){
-  //     $('#jv_classe').load('/filtro/ajax');
-  //   }else{
-  //     $('#jv_classe').load('/filtro/ajax/' + jsEstasdo);
-      
-  //   }
-  // }
-
-  // //RECEBENDO - ESTADO
-  //  document.getElementById("jsEnviaEstado").onchange = function() {filtroEstado(); filtroAjax()};
-  //  function filtroEstado() {
-  //      var jsFiltroEstado = document.getElementById("jsEnviaEstado").value;
-  //      document.getElementById("jsRecebeEstado").value = jsFiltroEstado;
-  //  }
-
-
-  //RECEBENDO ESTACAO
-  function filtroEstacao() {  
-      var jsAjaxFiltro = document.getElementById("jsAjaxFiltro");
-      var opcaoTexto = jsAjaxFiltro.options[jsAjaxFiltro.selectedIndex].text;
-      //alert(opcaoTexto);
-      document.getElementById("jsRecebeEstacao").value = opcaoTexto;
-  }
-
-  //RECEBENDO DATA CAPTAÇÃO MINIMA
-  document.getElementById("jsEnviaDataMin").onchange = function() {filtroDataMin()};
-  function filtroDataMin() {
-       var jsEnviaDataMin = document.getElementById("jsEnviaDataMin").value;
-       var jsData = jsEnviaDataMin.substring(0, 10);
-       var jsHora = jsEnviaDataMin.substring(11);
-       var jsHora = jsHora.substring(0,2);
-       var jsHora = jsHora + ':00:00';
-       document.getElementById("jsRecebeDataMin").value = jsData + ' ' + jsHora;
-  }
-
-    //RECEBENDO DATA CAPTAÇÃO MAXIMA
-    document.getElementById("jsEnviaDataMax").onchange = function() {filtroDataMax()};
-    function filtroDataMax() {
-         var jsEnviaDataMax = document.getElementById("jsEnviaDataMax").value;
-         var jsData = jsEnviaDataMax.substring(0, 10);
-         var jsHora = jsEnviaDataMax.substring(11);
-         var jsHora = jsHora.substring(0,2);
-         var jsHora = jsHora + ':00:00';
-         document.getElementById("jsRecebeDataMax").value = jsData + ' ' + jsHora;
+    if(jsFiltroData == ''){
+      //alert(jsFiltroData);
+    }else{
+      var jsAno = jsFiltroData.substring(0, 10);
+      var jsAno = jsAno.split('-').reverse().join('/');
+      var jsHora = jsFiltroData.substring(11);
+      var jsHora = jsHora.substring(0,2);
+      var jsHora = jsHora + ':00:00';
+      var jsConsolidado = jsAno + ' ' + jsHora;
+      document.getElementById("dtMax").innerHTML = jsConsolidado;
     }
+ }
 
+//TRATANDO DATA MINIMA - TOPO DA PAGINA
+function dtMin() {
 
-  // //TRATANDO DATA TOPO DA PAGINA
-  function jsFiltroDashboard() {
-
-      // var jsFiltroData = document.getElementById("jsRecebeData").value;
-      
-      // if(jsFiltroData == ''){
-      //   //alert(jsFiltroData);
-      // }else{
-      //   var jsAno = jsFiltroData.substring(0, 10);
-      //   var jsAno = jsAno.split('-').reverse().join('/');
-      //   var jsHora = jsFiltroData.substring(11);
-      //   var jsHora = jsHora.substring(0,2);
-      //   var jsHora = jsHora + ':00:00';
-      //   var jsConsolidado = jsAno + ' ' + jsHora;
-      //   document.getElementById("ano").innerHTML = jsConsolidado;
-      // }
-   }
+  var jsFiltroData = document.getElementById("jsRecebeDataMin").value;
+  
+  if(jsFiltroData == ''){
+    //alert(jsFiltroData);
+  }else{
+    var jsAno = jsFiltroData.substring(0, 10);
+    var jsAno = jsAno.split('-').reverse().join('/');
+    var jsHora = jsFiltroData.substring(11);
+    var jsHora = jsHora.substring(0,2);
+    var jsHora = jsHora + ':00:00';
+    var jsConsolidado = jsAno + ' ' + jsHora;
+    document.getElementById("dtMin").innerHTML = jsConsolidado;
+  }
+}
