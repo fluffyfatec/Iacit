f577eb3ff38785812d66814831f8601df6f2fcc8
ecampos14 <88864112+ecampos14@users.noreply.github.com>
2022-10-25 23:38:57 -0300

    Desenvolvimento task 108
    Mapeamento tabelas permissao e usuario

---
+++ API-IACIT/api/src/main/java/com/fluffyiacit/api/modal/PermissaoModal.java
@@ -0,0 +1,38 @@
+package com.fluffyiacit.api.modal;
+
+import java.math.BigDecimal;
+import java.sql.Timestamp;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import org.hibernate.annotations.ColumnDefault;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Entity(name= "permissao")
+@Table(name= "permissao")
+@Getter
+@Setter
+@ToString
+
+public class PermissaoModal {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name= "codPermissao", nullable = false)
+	private Integer codPermissao;
+	
+	@Column(name= "permissaoNome",length = 50, nullable = false)
+	private String permissaoNome;
+	
+	@Column(name= "permissaoDescricao",length = 200, nullable = false)
+	private String permissaoDescricao; 
+
+}
--- API-IACIT/api/src/main/java/com/fluffyiacit/api/modal/UsuarioModal.java
+++ API-IACIT/api/src/main/java/com/fluffyiacit/api/modal/UsuarioModal.java
@@ -1,11 +1,15 @@
 package com.fluffyiacit.api.modal;
 
 
+import java.security.Timestamp;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 import lombok.Getter;
@@ -24,11 +28,26 @@ public class UsuarioModal {
 	@Column(name = "codUsuario")
 	private Integer codUsuario;
 	
-	@Column(name = "nome")
-	private String nome;
+	@Column( name= "usuarioNome",length = 60,nullable = true)
+	private String usuarioNome ;
 	
-	@Column(name = "senha")
-	private String senha;
+	@Column( name= "usuarioUsername",length = 20,nullable = true)
+	private String usuarioUsername;
+	
+	@Column( name= "usuarioSenha",length = 20,nullable = true)
+	private String usuarioSenha ;
+	
+	@Column( name= "usuarioDatahoraCriacao",nullable = true)
+	private Timestamp usuarioDatahoraCriacao;
+	
+	@Column( name= "usuarioCadastrante",nullable = true)
+	private Integer usuarioCadastrante; 
+	
+	@ManyToOne
+    @JoinColumn(name = "permissao", referencedColumnName = "codPermissao")
+	private PermissaoModal codPermissao;
+	
+
 
 	
 
