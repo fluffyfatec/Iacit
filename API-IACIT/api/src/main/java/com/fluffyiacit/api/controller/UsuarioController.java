3e910cf3835afe21286318bba8372042eeb6f712
Patrick Souza <patrickgabriel103f@gmail.com>
2022-11-03 21:54:22 -0300

    Task-107
    Criacao UsuarioController

---
+++ API-IACIT/api/src/main/java/com/fluffyiacit/api/controller/UsuarioController.java
@@ -0,0 +1,28 @@
+package com.fluffyiacit.api.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.fluffyiacit.api.service.UsuarioService;
+
+import DTO.UsuarioDTO;
+
+@RestController
+@RequestMapping(value = "/cadastro")
+public class UsuarioController {
+    
+    @Autowired
+    UsuarioService usuarioService;
+    
+    @PostMapping
+    private ModelAndView cadastrar(@RequestBody UsuarioDTO usuario) {
+        
+       ModelAndView modelandview = usuarioService.criarUsuario(usuario);
+       return modelandview;
+    }
+
+}
