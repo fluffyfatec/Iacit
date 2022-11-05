76fdb719d0e4594add0bebc209b2bf0d06bebab1
ecampos14 <88864112+ecampos14@users.noreply.github.com>
2022-11-05 15:33:46 -0300

    continuacao task-108

--- API-IACIT/api/src/main/java/com/fluffyiacit/api/controller/UsuarioController.java
+++ API-IACIT/api/src/main/java/com/fluffyiacit/api/controller/UsuarioController.java
@@ -1,53 +1,14 @@
 package com.fluffyiacit.api.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-<<<<<<< Updated upstream
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.servlet.ModelAndView;
-=======
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
->>>>>>> Stashed changes
 
-import com.fluffyiacit.api.modal.UsuarioModal;
-import com.fluffyiacit.api.repository.UsuarioRepository;
 import com.fluffyiacit.api.service.UsuarioService;
 
-<<<<<<< Updated upstream
-@Controller
-@RequestMapping
-public class UsuarioController {
-
-	@Autowired(required = true)
-	private UsuarioService service;
-	
-	 @Autowired
-	 private UsuarioRepository repository;
-	
-	@RequestMapping(value = { "/Usuario" }, method = RequestMethod.GET)
-	public ModelAndView telaUsuario() {
-		ModelAndView modelAndView = new ModelAndView();
-		return modelAndView;
-	}
-
-	@RequestMapping(value = {"/deletar/{codUsuario}"} , method = RequestMethod.GET)
-	public ModelAndView deletar(@PathVariable("codUsuario") Integer codUsuario) {
-		ModelAndView modelAndView = new ModelAndView();
-		UsuarioModal usuario = repository.findById(codUsuario).get();
-		repository.deleteById(codUsuario);
-		return modelAndView;
-	}
-
-}
-=======
 @RestController
 @RequestMapping(value = "/cadastro")
 public class UsuarioController {
@@ -56,10 +17,9 @@ public class UsuarioController {
     UsuarioService usuarioService;
  
 
-    @PostMapping(value = "/deletar")
-    public void delete(@RequestBody String usuarioUsername ){
+    @PostMapping(value = "/deletar/ajax/{usuarioUsername}")
+    public void delete(@PathVariable("usuarioUsername") String usuarioUsername ){
 		usuarioService.deletar(usuarioUsername);
 	
     }
 }
->>>>>>> Stashed changes
--- API-IACIT/api/src/main/java/com/fluffyiacit/api/repository/UsuarioRepository.java
+++ API-IACIT/api/src/main/java/com/fluffyiacit/api/repository/UsuarioRepository.java
@@ -1,28 +1,12 @@
 package com.fluffyiacit.api.repository;
 
-<<<<<<< Updated upstream
-=======
-import java.sql.Timestamp;
-
-import javax.transaction.Transactional;
-
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
->>>>>>> Stashed changes
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.fluffyiacit.api.modal.UsuarioModal;
 
 
 @Repository
-<<<<<<< Updated upstream
-public interface UsuarioRepository extends CrudRepository<UsuarioModal, Integer> {
-
-}
-=======
 public interface UsuarioRepository extends JpaRepository<UsuarioModal, String> {
 
 	
@@ -38,4 +22,3 @@ public interface UsuarioRepository extends JpaRepository<UsuarioModal, String> {
 
 	
 }
->>>>>>> Stashed changes
