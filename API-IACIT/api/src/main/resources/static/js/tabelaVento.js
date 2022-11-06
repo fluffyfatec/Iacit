805303ca9bd7560ac3cb97a421309816e554430e
Ana Clara Oliveira Leal <heyanaleal@gmail.com>
2022-11-06 15:54:35 -0300

    update tela vento card tabela

--- API-IACIT/api/src/main/resources/static/js/tabelaVento.js
+++ API-IACIT/api/src/main/resources/static/js/tabelaVento.js
@@ -1,7 +1,7 @@
 // get the table element
 var $table = document.getElementById("tabelaVento"),
 // number of rows per page
-$n = 10,
+$n = 18,
 // number of rows of the table
 $rowCount = $table.rows.length,
 // get the first cell's tag name (in the first row)
