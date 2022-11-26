a20d0d4df0e02e5e8f08c1e02447c91f7cef99a6
tcamillo4-ctrlz <66873418+tcamillo4-ctrlz@users.noreply.github.com>
2022-11-26 15:49:18 -0300

    Update Task 128
    Alterado Tela Login
    Ciclo de paginas

--- API-IACIT/api/src/main/java/com/fluffyiacit/api/controller/LoginController.java
+++ API-IACIT/api/src/main/java/com/fluffyiacit/api/controller/LoginController.java
@@ -1,27 +1,39 @@
 package com.fluffyiacit.api.controller;
 
-import com.fluffyiacit.api.service.LoginService;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
+
+import com.fluffyiacit.api.service.LoginService;
 
 import DTO.LoginDTO;
 
 @RestController
-@RequestMapping(value = "/login")
 public class LoginController {
 
 	@Autowired(required = true)
 	LoginService loginService;
 	
+
+    @RequestMapping(value = { "/" }, method = RequestMethod.GET)
+	public ModelAndView PaginaIndex() {
+		ModelAndView modelAndView = new ModelAndView();
+		modelAndView.setViewName("login");
+		return modelAndView;
+		//
+	}
+
 	@PostMapping
 	public ModelAndView login(@RequestBody LoginDTO loginDTO) {
 		
 		return loginService.login(loginDTO);
-	}
+	}	
+
+
+
+
 }
--- API-IACIT/api/src/main/resources/static/js/login.js
+++ API-IACIT/api/src/main/resources/static/js/login.js
@@ -2,6 +2,16 @@ var txtusername = document.getElementById("txtusername");
 var txtpassword = document.getElementById("txtpassword");
 
 function login(){
-    console.log(txtusername.value)
-    console.log(txtpassword.value)
+
+    var username = txtusername.value;
+    var password = txtpassword.value;
+
+    if(username == "admin" && password == "admin"){
+        alert('Sucesso');
+        location.href = "/home";
+    }else{
+        alert('Usuario ou senha incorretos');
+    }
+
+
 }
