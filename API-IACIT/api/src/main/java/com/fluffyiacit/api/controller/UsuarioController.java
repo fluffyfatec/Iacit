ee100e65cd2d5ecc5919e3ac0ae0f2852c7be9bb
AldrikAlvaro <51426536+Aldrik-Alvaro@users.noreply.github.com>
2022-11-26 23:10:09 -0300

    ajustes finais

--- API-IACIT/api/src/main/java/com/fluffyiacit/api/controller/UsuarioController.java
+++ API-IACIT/api/src/main/java/com/fluffyiacit/api/controller/UsuarioController.java
@@ -1,14 +1,19 @@
 package com.fluffyiacit.api.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.fluffyiacit.api.modal.UsuarioModal;
+import com.fluffyiacit.api.repository.UsuarioRepository;
 import com.fluffyiacit.api.service.UsuarioService;
 
+import DTO.EstacaoDTO;
 import DTO.UsuarioDTO;
 
 @RestController
@@ -18,17 +23,66 @@ public class UsuarioController {
     @Autowired
     UsuarioService usuarioService;
     
-    @PostMapping
-    private ModelAndView cadastrar(@RequestBody UsuarioDTO usuario) {
-        
-    	
-       ModelAndView modelandview = usuarioService.criarUsuario(usuario);
-       return modelandview;
-    }
+    @Autowired
+    UsuarioRepository usuariorepository;
     
- 
-    
-	  
 
+    
+//	@RequestMapping(value = { "/editar" }, method = RequestMethod.POST)
+//	public ModelAndView editarStatus(UsuarioModal usuario) {
+//		ModelAndView modelAndView = new ModelAndView();
+//		// FILTROS DE DADOS
+//		usuario.getUsuarioNome();
+//		
+//		estacao.getEstacaoNome();
+//		estacao.getEstacaoEstado();
+//		estacao.getEstacaoStatus();
+//		System.out.println(estacao.getEstacaoNome());
+//		System.out.println(estacao.getEstacaoEstado());
+//		System.out.println(estacao.getEstacaoStatus());
+//		
+//		return adm.update(estacao);
+//	}
+
+	//LISTAR EDITAR
+	@RequestMapping(value = { "/editar/{usuarioUsername}" }, method = RequestMethod.GET)
+	public ModelAndView editar(@PathVariable("usuarioUsername") String usuarioUsername) {
+		UsuarioModal usuario = new UsuarioModal();
+		usuario.setUsuarioUsername(usuarioUsername);
+		return usuarioService.listar(usuario);
+	}
+	
+	
+	//CADASTRAR
+	@RequestMapping(value = { "/cad/{usuario_nome}/{usuario_username}/{usuario_senha}/{nome_permissao}/{usuario_adm}/{usuario_alterou}" }, method = RequestMethod.GET)
+	public ModelAndView filtroAjax1(@PathVariable("usuario_nome") String usuario_nome,
+            					   	@PathVariable("usuario_username") String usuario_username,
+            					   	@PathVariable("usuario_senha") String usuario_senha,
+            					   	@PathVariable("nome_permissao") String nome_permissao,
+            					   	@PathVariable("usuario_adm") String usuario_adm,
+            					   	@PathVariable("usuario_alterou") String usuario_alterou) {
+		UsuarioDTO usuario = new UsuarioDTO();
+		usuario.setUsuario_nome(usuario_nome);
+		usuario.setUsuario_username(usuario_username);
+		usuario.setUsuario_senha(usuario_senha);
+		usuario.setNome_permissao(nome_permissao);
+		usuario.setUsuario_adm(usuario_adm);
+		usuario.setUsuario_alterou(usuario_alterou);
+		return usuarioService.criarUsuario(usuario);
+	}
+	
+
+	//FILTRO AJAX - USUARIO
+	@RequestMapping(value = { "/usu/{id}" }, method = RequestMethod.GET)
+	public ModelAndView filtroAjaxUsuario(@PathVariable("id") String id) {
+		ModelAndView modelAndView = new ModelAndView();
+
+		List<UsuarioModal> filtrousuario = usuariorepository.listarusuario(id);
+		modelAndView.addObject("filtrousuario", filtrousuario);
+		
+		modelAndView.setViewName("AjaxTabUsuario");
+		return modelAndView;
+	}
+	
 }
 
--- API-IACIT/api/src/main/java/com/fluffyiacit/api/repository/UsuarioRepository.java
+++ API-IACIT/api/src/main/java/com/fluffyiacit/api/repository/UsuarioRepository.java
@@ -1,14 +1,20 @@
 package com.fluffyiacit.api.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.fluffyiacit.api.modal.UsuarioModal;
 
 @Repository
 public interface UsuarioRepository extends JpaRepository<UsuarioModal, String> {
+	@Query("SELECT v FROM usuario v WHERE v.usuarioNome = :usuarioNome")
+	public List<UsuarioModal> listarusuario(@Param("usuarioNome") String usuarioNome);
 	
+
 	UsuarioModal findByUsuarioUsername(String usuarioUsername);
- 
+
 }
